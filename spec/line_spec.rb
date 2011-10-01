e47f0c4ce0777a3e5752c2771c3498568df5625d
edhana <edhana@gmail.com>
2011-10-01 20:10:18 -0300

    Estou no meio do caminho dos testes para descobrir o cruzamento real das retas

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -7,8 +7,11 @@ describe Line do
 		@pointA = {:x => 2, :y => 1}
 		@pointB = {:x => 4, :y => 3}
 		#second semi-line
-		@pointC = {:x => 2, :y => 4}
-		@pointD = {:x => 3, :y => 1}
+		@pointC = {:x => 3, :y => 1}
+		@pointD = {:x => 2, :y => 4}
+		#third semi-line
+		@pointE = {:x => 3, :y => 0}
+		@pointF = {:x => 1, :y => 2}
 	end
 
 	it "should find the gradient of the straight line" do
@@ -16,13 +19,27 @@ describe Line do
 		sl.gradient.should be_eql(1.to_f)
 	end
 
-	# it "should create a straight with the points" do
-	# 	sl1 = Line.create_new(@pointA, @pointB)
-	# 	sl1.gradient.should be_eql(2/3.to_f)
-	# end
+	it "should find the linear touch of the line" do
+		sl = Line.new(@pointA, @pointB)
+		sl.linear_touch.should be_eql(-1.to_f)
 
-	it "should complete the coefficients of the line" do
-		# sl1 = Line.new(@pointA, @pointB)
-		# sl1.gradient.should be_eql(2/3.to_f)
+		# Double Test
+		sl2 = Line.new(@pointE, @pointF)
+		sl2.linear_touch.should be_eql(3.to_f)
+	end
+
+	it "should include anotther point from the line" do
+		pointC = {:x => 3, :y => 2}
+		sl = Line.new(@pointA, @pointB)
+		sl.include?(pointC).should be_true
+	end
+
+	it "should intercept with the other line (3,0) and (1,2)" do
+		sl = Line.new(@pointA, @pointB)
+		sl2 = Line.new(@pointC, @pointD)
+		sl.intercept?(sl2).should be_true
+		sl3 = Line.new(@pointE, @pointF)
+		sl.intercept?(sl3).should be_true
+
 	end
 end
--- src/geometry.rb
+++ src/geometry.rb
@@ -2,9 +2,11 @@ class Line
 	def initialize(pointA, pointB)
 		@a, @b, @c = nil, nil, nil
 		@gradient = nil
+		@linear = nil
 		@pointA = pointA
 		@pointB = pointB
 		calculate_gradient
+		calculate_linear
 	end	
 
 	def gradient(ac = nil)
@@ -27,11 +29,39 @@ class Line
 		end
 	end
 
-	def intercept?(line)
+	def intercept?(line)									
+		x = calculate_x_from_two_lines(self, line)
+		y = (@gradient*@pointA[:x].to_f) + @linear
+
+		new_point = { :x => x, :y => y}
+
+		puts "========> New Point x: #{x} - y: #{y}"
+		if include?(new_point)
+			puts "========> Fodeo!"
+			true
+		else
+			false
+		end
+	end
+
+	def linear_touch
+		@linear
 	end
 
 	private
 	def calculate_gradient
 		@gradient = (@pointB[:y] - @pointA[:y]) / (@pointB[:x] - @pointA[:x]).to_f
+	end
+
+	def calculate_linear
+		@linear = nil
+		if not @gradient.nil?
+			@linear =  @pointA[:y].to_f - (@gradient * @pointA[:x].to_f )
+		end
+		@linear
+	end
+
+	def calculate_x_from_two_lines(line1, line2)
+		(line2.linear_touch - line1.linear_touch)/(line1.gradient - line2.gradient)
 	end
 end
